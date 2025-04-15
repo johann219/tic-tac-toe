c879ebc57c9367fd29d24bf5ca60c91dac47e97e
johann219 <147496124+johann219@users.noreply.github.com>
2025-04-15 23:34:34 +0300

    Add logic for ongoing game and continious asking for next move

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const createPlayer = (name, symbol) => {
+    const playerName = name;
+    const playerSymbol = symbol;
+
+    return { playerName, playerSymbol };
+};
+
+
 const GameBoard = (() => {
     const state = [[null, null, null], [null, null, null], [null, null, null]];
 
@@ -11,17 +19,44 @@ const GameBoard = (() => {
     return { state, changeState };
 })();
 
-const createPlayer = (name, symbol) => {
-    const playerName = name;
-    const playerSymbol = symbol;
+const Game = (() => {
+    const player1 = createPlayer('Ivan', 'x');
+    const player2 = createPlayer('Vika', 'o');
 
-    return { playerName, playerSymbol };
-};
+    let gameOngoing = false;
 
-const move = prompt('player1, enter your move (2 digits separated by space): ');
+    const askPlayerForMove = (player) => {
+        const moveStr = prompt(`${player.playerName}, enter your move (2 digits separated by space): `);
+        const row = moveStr.split(' ').at(0);
+        const column = moveStr.split(' ').at(1);
+        return { row, column };
+    }; 
 
-const createMove = (moveStr) => {
-    const row = moveStr.split(' ').at(0);
-    const column = moveStr.split(' ').at(1);
-    return { row, column };
-};
+    const startGame = () => {
+        console.log(`Game starts! ${player1.playerName} is playing as ${player1.playerSymbol} against ${player2.playerName} who is playing as ${player2.playerSymbol}`);
+
+        gameOngoing = true;
+        let currentPlayer = player1;
+        let currentTurn = 1;
+
+        while (gameOngoing) {
+            console.log(`Turn ${currentTurn}`);
+            console.log(`${currentPlayer.playerName} moves!`);
+
+            const move = askPlayerForMove(currentPlayer);
+            GameBoard.changeState(move, currentPlayer.playerSymbol);
+
+            // check for win
+                // declare winner
+            // check for draw
+                // declare draw
+            // change current player
+
+            console.log(currentPlayer === player1);
+        }
+    };
+
+    return { startGame };
+})();
+
+Game.startGame();
